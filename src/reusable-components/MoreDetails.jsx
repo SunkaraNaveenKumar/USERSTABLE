0c905f921d876f3a3f7bb15805b7a9ef3a800554
naveen <sunkaranaveensunkara@gmail.com>
2024-11-02 13:29:29 +0530

    user-list/add phone-number and other details

--- src/reusable-components/MoreDetails.jsx
+++ src/reusable-components/MoreDetails.jsx
@@ -7,13 +7,21 @@ const MoreDetails = () => {
     <div className=" border">
       {Object.keys(selectedUser).length ? (
         Object.entries(selectedUser)?.map(([key, value]) => {
-          if (typeof value !== "object")
+          if (typeof value !== "object") {
             return (
               <div className="flex row gap-x-2 bg-red-100 rounded-md">
                 <p>{key}</p>
                 <p>{value}</p>
               </div>
             );
+          } else {
+            return (
+              <div className="flex row gap-x-2 bg-red-100 rounded-md">
+                <p>{key}</p>
+                <p>{JSON.stringify(value)}</p>
+              </div>
+            );
+          }
         })
       ) : (
         <p>Please click on view more to get the more details</p>
